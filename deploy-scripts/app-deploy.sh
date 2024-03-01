753cd27dfe8273d8bad0120437e0607c7799f9c1
anantakumarghosh <antaghosh@gmail.com>
2024-03-01 16:30:31 +0530

    ci(config): :rocket: enhance deploy scripts
    improve deploy scripts
    
    Ref: #13

--- deploy-scripts/app-deploy.sh
+++ deploy-scripts/app-deploy.sh
@@ -17,16 +17,15 @@ create_folder_if_not_exists() {
         echo "Folder created: $folder_path"
     fi
 }
-
 echo "$REPONAME-DEPLOY SCRIPT STARTED."
 echo "========================="
 echo "CHANGING WORKING DIRECTORY TO $PROJECT_DEPLOYMENT_PATH/temp TO DOWNLOAD LATEST RELEASE OF $REPONAME."
-create_folder_if_not_exists "$PROJECT_DEPLOYMENT_PATH/temp"
+create_folder_if_not_exists "$PROJECT_DEPLOYMENT_PATH"/temp
 echo "CLEANING UP PREVIOUS RELEASE ARTIFACT..."
-echo "$PROJECT_DEPLOYMENT_PATH/temp/"
-sudo rm -rf "$PROJECT_DEPLOYMENT_PATH/temp/web*.zip"
+echo "$PROJECT_DEPLOYMENT_PATH"/temp/
+sudo rm -rf "$PROJECT_DEPLOYMENT_PATH"/temp/web*.zip
 echo "========================="
-cd "$PROJECT_DEPLOYMENT_PATH/temp" || exit
+cd "$PROJECT_DEPLOYMENT_PATH"/temp || exit
 echo "========================="
 echo "CHANGED DIRECTORY. \nCURRENT WORKING DIRECTORY: ""$PWD"""
 echo "========================="
@@ -35,7 +34,7 @@ echo "SHOW ALL FILES:"
 ls -al
 echo "========================="
 echo "STARTING DOWNLOAD OF $REPONAME ..."
-gh release download --pattern "web*.zip" -R $OWNER_NAME/$REPONAME
+gh release download --pattern "web*.zip" -R "$OWNER_NAME"/"$REPONAME"
 echo "========================="
 echo "DOWNLOADED FILES:"
 ls -al
@@ -44,24 +43,24 @@ echo "STARTING SETUP PROCESS OF NEW DOWNLOADED $REPONAME"
 echo "========================="
 echo "CURRENT WORKING DIRECTORY: " $PWD
 echo "========================="
-create_folder_if_not_exists "$PROJECT_DEPLOYMENT_PATH/$REPONAME"
+create_folder_if_not_exists "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"
 echo "========================="
 echo "SHOW ALL FILES:"
 ls -al 
 echo "CLEANING UP OLD SETUP OF $REPONAME ..."
-echo $PROJECT_DEPLOYMENT_PATH/$REPONAME/
-sudo rm -rf $PROJECT_DEPLOYMENT_PATH/$REPONAME/*
+echo "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"/
+sudo rm -rf "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"/*
 echo "========================="
 echo "CLEANED OLD SETUP OF $REPONAME."
 echo "========================="
-echo "CHANGING WORKING DIRECTORY TO ""$PROJECT_DEPLOYMENT_PATH""/$REPONAME"
+echo CHANGING WORKING DIRECTORY TO "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"
 cd "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME" || exit
 echo "========================="
 echo "SHOW ALL FILES:"
 ls -al
 echo "========================="
 echo "COPYING NEW ARTIFICAT FROM ""$PROJECT_DEPLOYMENT_PATH""/temp/ TO ""$PROJECT_DEPLOYMENT_PATH""/$REPONAME..."
-sudo cp $PROJECT_DEPLOYMENT_PATH/temp/web*.zip "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"/
+sudo cp "$PROJECT_DEPLOYMENT_PATH"/temp/web*.zip "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"/
 echo "========================="
 echo "COPIED NEW ARTIFACT."
 echo "SHOW ALL FILES:"
--- deploy-scripts/service-deploy.sh
+++ deploy-scripts/service-deploy.sh
@@ -24,7 +24,7 @@ echo "CHANGING WORKING DIRECTORY TO $PROJECT_DEPLOYMENT_PATH/temp TO DOWNLOAD LA
 create_folder_if_not_exists "$PROJECT_DEPLOYMENT_PATH/temp"
 echo "CLEANING UP PREVIOUS RELEASE ARTIFACT..."
 echo "$PROJECT_DEPLOYMENT_PATH/temp/"
-sudo rm -rf "$PROJECT_DEPLOYMENT_PATH/temp/service*.zip"
+sudo rm -rf "$PROJECT_DEPLOYMENT_PATH"/temp/service*.zip
 echo "========================="
 cd "$PROJECT_DEPLOYMENT_PATH/temp" || exit
 echo "========================="
@@ -44,24 +44,24 @@ echo "STARTING SETUP PROCESS OF NEW DOWNLOADED $REPONAME"
 echo "========================="
 echo "CURRENT WORKING DIRECTORY: " $PWD
 echo "========================="
-create_folder_if_not_exists "$PROJECT_DEPLOYMENT_PATH/$REPONAME"
+create_folder_if_not_exists "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"
 echo "========================="
 echo "SHOW ALL FILES:"
 ls -al 
 echo "CLEANING UP OLD SETUP OF $REPONAME ..."
-echo $PROJECT_DEPLOYMENT_PATH/$REPONAME/
-sudo rm -rf $PROJECT_DEPLOYMENT_PATH/$REPONAME/*
+echo "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"/
+sudo rm -rf "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"/*
 echo "========================="
 echo "CLEANED OLD SETUP OF $REPONAME."
 echo "========================="
-echo "CHANGING WORKING DIRECTORY TO ""$PROJECT_DEPLOYMENT_PATH""/$REPONAME"
+echo CHANGING WORKING DIRECTORY TO "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"
 cd "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME" || exit
 echo "========================="
 echo "SHOW ALL FILES:"
 ls -al
 echo "========================="
 echo "COPYING NEW ARTIFICAT FROM ""$PROJECT_DEPLOYMENT_PATH""/temp/ TO ""$PROJECT_DEPLOYMENT_PATH""/$REPONAME..."
-sudo cp $PROJECT_DEPLOYMENT_PATH/temp/service*.zip "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"/
+sudo cp "$PROJECT_DEPLOYMENT_PATH"/temp/service*.zip "$PROJECT_DEPLOYMENT_PATH"/"$REPONAME"/
 echo "========================="
 echo "COPIED NEW ARTIFACT."
 echo "SHOW ALL FILES:"
